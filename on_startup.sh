e6e9bbfbd8b1ad3c1d3126a0ffb635b1b4a03677
Bent Müller <bent.hochrad@gmail.com>
2020-11-12 14:10:07 +0100

    startup script written

---
+++ on_startup.sh
@@ -0,0 +1,7 @@
+xrandr --output DP-0 --auto --output HDMI-0 --auto --scale 1.5x1.5
+
+xset r rate 220 40
+
+nitrogen --set-zoom-fill ~/Documents/Wallpapers/Light\ Stream\ Blue.jpg
+
+compton -cCGfF -b -i 0.75
--- setup.sh
+++ setup.sh
@@ -2,5 +2,3 @@ cp .bash_profile ~/.zshenv
 cp .bash_profile ~/.bash_profile
 chmod +x ~/.bash_profile
 cp vimrc ~/.vimrc
-cp vimrcChange ~/vimrcChange
-chmod +x ~/vimrcChange
