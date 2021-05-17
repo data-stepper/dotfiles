a4787fa3cea0196a76f3eb3a72c02954259434b4
data-stepper <bent.hochrad@gmail.com>
2021-05-17 18:45:16 +0200

    Added more to the setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 cp vimrc ~/.vimrc
+cp xinitrc ~/.xinitrc
+cp Xresources ~/.Xresources
+
+mkdir ~/.config/i3
+cp i3-config ~/.config/i3/config
 
 # Make snippet directory and copy code snippets over
+mkdir ~/.vim
+mkdir ~/.vim/UltiSnips
 cp ./snippets/* ~/.vim/UltiSnips/
 
