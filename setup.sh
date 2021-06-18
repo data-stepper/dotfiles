a07a7161e0bb51296c09c3bfe7eead3522d9c78a
Bent Mueller <bent.hochrad@gmail.com>
2021-06-18 12:20:49 +0200

    Simple bash install script now creates directories too

--- setup.sh
+++ setup.sh
@@ -1,12 +1,26 @@
-cp vimrc ~/.vimrc
-cp xinitrc ~/.xinitrc
-cp Xresources ~/.Xresources
+# Copy some config files
+cp .zshrc ~/.zshrc
+cp .xinitrc ~/.xinitrc
+cp .Xresources ~/.Xresources
 
-mkdir ~/.config/i3
+# Create i3 config directory and file
+mkdir -p ~/.config/i3
 cp i3-config ~/.config/i3/config
 
 # Make snippet directory and copy code snippets over
-mkdir ~/.vim
-mkdir ~/.vim/UltiSnips
+echo "Setting up vim now..."
+cp .vimrc ~/.vimrc
+
+# Copy vim snippets over
+mkdir -p ~/.vim/UltiSnips
 cp ./snippets/* ~/.vim/UltiSnips/
+
+# Coc config file
+cp ./coc-settings.json ~/.vim/coc-settings.json
+
+# Setup compton composition manager
+cp ./compton.conf ~/.config/compton.conf
+
+# Copy konsole config file
+cp ./konsolerc ~/.config/konsolerc
 
