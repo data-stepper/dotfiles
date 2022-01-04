1d85c83b2d02f6d962de74a4a2628a1a81bacc3d
Bent Mueller <bent.hochrad@gmail.com>
2022-01-04 19:26:02 +0100

    Finish this script, just 5 minutes

--- scripts/screenshot-anki-card-trigger.py
+++ scripts/screenshot-anki-card-trigger.py
@@ -2,6 +2,20 @@ from os import popen
 from os.path import normpath
 from pathlib import Path
 
+screenshot_dir = Path.home() / "screenshots"
+screenshot_dir.mkdir(exist_ok=True)
+
+csv_file_path = screenshot_dir / "screenshots_data.csv"
+
+if not csv_file_path.exists():
+    # Create the csv file
+    with csv_file_path.open("w") as f:
+
+        # Write the column names
+        f.write("image_path,description\n")
+
+# Now the csv file definitely exists
+
 clipboard = popen("xclip -o").read()
 
 path = Path(clipboard)
