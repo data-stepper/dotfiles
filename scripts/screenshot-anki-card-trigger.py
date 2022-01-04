9b2d0385ce583b0af2e2257c3d2809bcb50939d0
Bent Mueller <bent.hochrad@gmail.com>
2022-01-04 23:33:37 +0100

    Script saves descriptions captured from primary selection correctly to csv file and takes the corresponding screenshot

--- scripts/screenshot-anki-card-trigger.py
+++ scripts/screenshot-anki-card-trigger.py
@@ -1,6 +1,7 @@
-from os import popen
+from os import popen, system
 from os.path import normpath
 from pathlib import Path
+from datetime import datetime
 
 screenshot_dir = Path.home() / "screenshots"
 screenshot_dir.mkdir(exist_ok=True)
@@ -18,11 +19,24 @@ if not csv_file_path.exists():
 
 clipboard = popen("xclip -o").read()
 
-path = Path(clipboard)
-path = Path.home() / (str(path) + "_scrot.png")
-path = normpath(path)
+description = '"' + clipboard.replace('"', '\\"') + '"'
 
-# This is actually a valid path now
+image_name = datetime.now().strftime("%Y-%m-%d---%H-%M-%S") + ".png"
 
+# Take the screenshot and afterwards write the entry to the csv file
+screenshot_command = "scrot -s $HOME/screenshots/{}".format(image_name)
 
-print(path)
+rval = system(screenshot_command)
+
+assert (
+    rval == 0
+), "Error occurred taking screenshot, not saving description to csv file"
+
+# Otherwise save description to csv file
+
+with csv_file_path.open("a") as f:
+    f.write("{},{}\n".format(image_name, description))
+
+print(description)
+print(screenshot_command)
+print("Successfully written everything to files")
