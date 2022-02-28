c1670a8ef41743ac67cd671b464a5c5998733969
Bent Mueller <bent.hochrad@gmail.com>
2022-02-28 09:33:35 +0100

    Added half install already

--- arch_bootstrap.bash
+++ arch_bootstrap.bash
@@ -5,11 +5,6 @@
 
 echo "Welcome to Bent's arch bootstrap script\n"
 
-# Use openssl as it is installed by default
-ROOT_PASSWORD=$(openssl rand -base64 24)
-
-read -r -p "Your root password is '$ROOT_PASSWORD', please write it down" -a arr
-
 read -r -p "Please enter your username: " -a arr
 HOSTNAME=${arr[0]}
 
@@ -19,9 +14,54 @@ USERPASSWORD=${arr[0]}
 read -r -p "Please enter the diskname you want to install arch linux on: " -a arr
 DISKNAME=${arr[0]}
 
-# TODO: Here pipe in the commands to partition the disks with gdisk
+# Use openssl as it is installed by default
+# Use it to generate a safe password
+ROOT_PASSWORD=$(openssl rand -base64 24)
+
+read -r -p "Your root password is '$ROOT_PASSWORD', please write it down" -a arr
+
+DISKNAME=nvme0n1
+
 # Commands taken from here: https://wiki.archlinux.de/title/Gdisk
 printf "o\ny\nn\n\n\n+512M\nef00\nn\n\n\n\n8300\nw\ny\n" | gdisk /dev/$DISKNAME
+
+# Now create the file systems
+
+# Create the filesystem for EFI partition
+mkfs.fat -F 32 -n BOOT /dev/$DISKNAME'p1'
+mkfs.ext4 -L ROOT /dev/$DISKNAME'p2'
+
+# Possibly also create swap but we usually don't need it
+# Note that this would require creating a swap partition above
+# mkswap -L SWAP /dev/SWAP_PARTITION
+
+# Mount the partitions
+mount -L ROOT /mnt
+
+mkdir /mnt/boot
+mount -L BOOT /mnt/boot
+
+# Now install some basic packages
+yes | pacstrap /mnt base base-devel linux linux-firmware dhcpcd nano vim
+
+# --> Change this if you have an amd cpu <-- !!
+yes | pacman --root /mnt -S intel-ucode
+# yes | pacman --root /mnt -S amd-ucode
+
+yes | pacman --root /mnt -S iwd
+
+# Generate file system table (fstab)
+genfstab -U /mnt > /mnt/etc/fstab
+
+# Chroot into root file system
+arch-chroot /mnt
+
+echo $USERNAME > /etc/hostname
+
+# Create locales
+echo LANG=en_US.UTF-8 > /etc/locale.conf
+
+
 
 
 
