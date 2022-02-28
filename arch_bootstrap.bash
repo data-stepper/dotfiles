e414933702b82b21c3c90e0fd8317d9ea309a8e4
Bent Mueller <bent.hochrad@gmail.com>
2022-02-28 10:30:10 +0100

    ln not lf

--- arch_bootstrap.bash
+++ arch_bootstrap.bash
@@ -67,7 +67,7 @@ echo en_US.UTF-8 UTF-8 >> /etc/locale.gen
 locale-gen
 
 # Set the local time to Berlin
-lf -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 
 # Create initramfs
 mkinitcpio -p linux
