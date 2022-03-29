c5d993be581448577f25b1b4ebee89986edd5caa
data-stepper <bent@gmail.com>
2022-03-29 14:39:31 +0200

    Fixed issue where it would copy the entire dotfiles contents to the home directory

--- installation_scripts/setup.sh
+++ installation_scripts/setup.sh
@@ -1,12 +1,13 @@
 # Get user home directory
 HOME_DIR=$(eval echo ~$user)
+DOTFILES_DIR=$HOME_DIR/git/dotfiles
 
 # Inform user of the install
-echo "Installing dotfiles to $HOME_DIR"
+echo "Installing dotfiles from $DOTFILES_DIR to $HOME_DIR"
 
 # Copy all config files now
 # This copies all config files recursively to the user's home directory
-cp -Rf ../home_dir/.* $HOME_DIR
+cp -Rf $DOTFILES_DIR/home_dir/. $HOME_DIR
 
 # Install vim-plug as plugin manager for neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
@@ -17,7 +18,7 @@ sudo cp ../utilitiy_scripts/autorunpy /usr/bin/
 sudo cp ../utilitiy_scripts/autotestpy /usr/bin/
 
 # Create screenshot directory
-mkdir -p ~/screenshots
+mkdir -p $HOME_DIR/screenshots
 
 # Copy screenshot scripts
 sudo cp ../utilitiy_scripts/screenshot-anki-card-trigger.py /bin
