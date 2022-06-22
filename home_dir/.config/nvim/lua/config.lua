1634bad8bc3009e1699448341a657389f92f6c83
data-stepper <bent@gmail.com>
2022-06-22 18:57:45 +0200

    Added sumneko lua language server

--- home_dir/.config/nvim/lua/config.lua
+++ home_dir/.config/nvim/lua/config.lua
@@ -397,6 +397,32 @@ for _, lsp in ipairs(servers) do
   }
 end
 
+-- Lua language server needs some special setup
+lspconfig.sumneko_lua.setup {
+  on_attach = on_attach,
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = "LuaJIT"
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {"vim"}
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true)
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false
+      }
+    }
+  }
+}
+
 -- Setup texlab extra as it is not really a language server.
 -- lspconfig["texlab"].setup {}
 
